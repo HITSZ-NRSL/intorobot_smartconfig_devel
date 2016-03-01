aca0152a9ec0b3a95f71b99b2f79290fe9019023
haoyao chen <hychen5@hitsz.edu.cn>
2016-03-01 10:03:21 +0800

    fix the undefined reference error

--- files/src/aircrack-ng.h
+++ files/src/aircrack-ng.h
@@ -261,5 +261,7 @@ struct WPA_data {
 
 void show_wep_stats( int B, int force, PTW_tableentry table[PTW_KEYHSBYTES][PTW_n], int choices[KEYHSBYTES], int depth[KEYHSBYTES], int prod );
 
+extern inline int wpa_send_passphrase(char *key, struct WPA_data* data, int lock);
+extern inline int wpa_receive_passphrase(char *key, struct WPA_data* data); 
 
 #endif /* _AIRCRACK_NG_H */
