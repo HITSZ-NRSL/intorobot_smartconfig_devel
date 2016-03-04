81081ca77d61a506b07fdede38cf6bf858cd5e6e
Haoyao Chen <hychen5@hitsz.edu.cn>
2016-03-04 22:28:53 +0800

    update files

---
+++ files/src/smartconfig-server.c
@@ -0,0 +1,56 @@
+/* 
+ * File:   smartconfig-server.c
+ * Author: Haoyao Chen
+ *
+ * Created on 2016年03月04日, 下午10:34
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int port=5557;
+
+int main(int argc, char** argv) {
+
+    int sin_len;
+    char message[256];
+    int iter;
+    int socket_descriptor;
+    struct sockaddr_in sin;
+    printf("Waiting for data form sender \n");
+
+    bzero(&sin,sizeof(sin));
+    sin.sin_family=AF_INET;
+    sin.sin_addr.s_addr=htonl(INADDR_ANY);
+    sin.sin_port=htons(port);
+    sin_len=sizeof(sin);
+
+    socket_descriptor=socket(AF_INET,SOCK_DGRAM,0);
+    bind(socket_descriptor,(struct sockaddr *)&sin,sizeof(sin));
+
+    while(1)
+    {
+        recvfrom(socket_descriptor,message,sizeof(message),0,(struct sockaddr *)&sin,&sin_len);
+        //  printf("%s, %s, %s, %d, %d " , str_ip, ap_ssid, ap_passwd, total_length, ssidpasswd_length);
+    	for(iter = 0; iter<12; iter++)
+    		printf("%02X ", (unsigned char)message[iter]);
+    	
+        if(strncmp(message,"stop",4) == 0)//接受到的消息为 “stop”
+        {
+
+            printf("Sender has told me to end the connection\n");
+            break;
+        }
+    }
+
+    close(socket_descriptor);
+    exit(0);
+
+    return (EXIT_SUCCESS);
+}
